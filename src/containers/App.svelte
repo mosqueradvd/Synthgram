eb3a324d2c8e70476daa892b025843159f0136b1
David Mosquera <mosqueradavid@outlook.es>
2020-04-25 23:59:27 -0500

    Initial commit

---
+++ src/containers/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import Timeline from "../components/Timeline.svelte";
+
+  let data = {};
+  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Fjalla+One&family=Lato:wght@300;400&display=swap");
+
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 39, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<Header />
+<Main>
+  <Timeline posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
